9e0940376ecadce4effcf9827ef2e4afbc59dc57
Gaurav <gaurav.raj@knoldus.com>
2022-01-14 22:47:18 +0530

    "comments added"

--- src/main/scala/knoldus/assignment2/FactRec.scala
+++ src/main/scala/knoldus/assignment2/FactRec.scala
@@ -2,7 +2,7 @@ package knoldus.assignment2
 
 class FactRec {
   def factUseRec(number:Int):BigInt = {
-    if(number<=1)  1
+    if(number<=1)  1   //if number is less than or equal to 1 then it will return 1
     else {
       val result= number*factUseRec(number-1)
       result
--- src/main/scala/knoldus/assignment2/FactTailRec.scala
+++ src/main/scala/knoldus/assignment2/FactTailRec.scala
@@ -6,8 +6,8 @@ class FactTailRec {
   def factUseTailRec(number:Int):BigInt = {
     @tailrec
     def helper(number:Int, accumulator:BigInt):BigInt= {
-      if(number<=1)  accumulator
-      else helper(number-1,number*accumulator)
+      if(number<=1)  accumulator    //if number is 0 then it will return result that is store in accumalator
+      else helper(number-1,number*accumulator) //in default case we are recursively calling our factorial function until we found our expected result
     }
     helper(number,1)
   }
