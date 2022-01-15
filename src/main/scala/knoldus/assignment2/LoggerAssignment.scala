a66ec25a9547f3a55bcb32fd6ceb3be1c23ad6d4
Gaurav <gaurav.raj@knoldus.com>
2022-01-15 23:16:51 +0530

    "test case"

--- src/main/scala/knoldus/assignment2/LoggerAssignment.scala
+++ src/main/scala/knoldus/assignment2/LoggerAssignment.scala
@@ -1,10 +1,8 @@
 package knoldus.assignment2
-import org.slf4j.{Logger, LoggerFactory}
+
 
 class LoggerAssignment{
-  val logger:Logger = LoggerFactory.getLogger(classOf[LoggerAssignment])
   def printLog(message:String, level:String = "INFO"):String ={
-    logger.info("["+level+"]:"+message)
     "["+level+"]:"+message
   }
 }
